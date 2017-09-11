0301ee0a7be4b048b43f1fb200f78c1da4f1fba1
Igor Almeida <igor.p.almeida@gmail.com>
2017-09-11 15:28:48 -0300

    lint free

--- server/test-query.py
+++ server/test-query.py
@@ -1,8 +1,4 @@
-"""
-testing api queries
-"""
-
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta
 from pytest import raises
 
 def test_query_tail():
@@ -55,36 +51,35 @@ def test_ag_selectedor():
     """
     from query import get_ag_selector
 
-    d1 = datetime(2017, 1, 1, 0, 0, 0)
-    d2 = datetime(2017, 1, 1, 17, 30, 15)
+    date1 = datetime(2017, 1, 1, 0, 0, 0)
+    date2 = datetime(2017, 1, 1, 17, 30, 15)
 
     # minutes
-    assert get_ag_selector(d1 - timedelta(seconds=1), d1) == "2016-12-31_23:59|2017-01-01_00:00"
-    assert get_ag_selector(d1 - timedelta(seconds=5), d1) == "2016-12-31_23:59|2017-01-01_00:00"
-    assert get_ag_selector(d1 - timedelta(seconds=59), d1) == "2016-12-31_23:59|2017-01-01_00:00"
-    assert get_ag_selector(d2 - timedelta(seconds=1), d2) == "2017-01-01_17:30"
-    assert get_ag_selector(d2 - timedelta(seconds=16), d2) == "2017-01-01_17:29|2017-01-01_17:30"
+    assert get_ag_selector(date1 - timedelta(seconds=1), date1) == "2016-12-31_23:59|2017-01-01_00:00"
+    assert get_ag_selector(date1 - timedelta(seconds=5), date1) == "2016-12-31_23:59|2017-01-01_00:00"
+    assert get_ag_selector(date1 - timedelta(seconds=59), date1) == "2016-12-31_23:59|2017-01-01_00:00"
+    assert get_ag_selector(date2 - timedelta(seconds=1), date2) == "2017-01-01_17:30"
+    assert get_ag_selector(date2 - timedelta(seconds=16), date2) == "2017-01-01_17:29|2017-01-01_17:30"
 
-    assert get_ag_selector(d1 - timedelta(seconds=60), d1) == "2016-12-31_23|2017-01-01_00"
-    assert get_ag_selector(d1 - timedelta(seconds=60 * 59), d1) == "2016-12-31_23|2017-01-01_00"
-    assert get_ag_selector(d2 - timedelta(seconds=60), d2) == "2017-01-01_17"
-    assert get_ag_selector(d2 - timedelta(seconds=60 * 60 - 1), d2) == "2017-01-01_16|2017-01-01_17"
+    assert get_ag_selector(date1 - timedelta(seconds=60), date1) == "2016-12-31_23|2017-01-01_00"
+    assert get_ag_selector(date1 - timedelta(seconds=60 * 59), date1) == "2016-12-31_23|2017-01-01_00"
+    assert get_ag_selector(date2 - timedelta(seconds=60), date2) == "2017-01-01_17"
+    assert get_ag_selector(date2 - timedelta(seconds=60 * 60 - 1), date2) == "2017-01-01_16|2017-01-01_17"
 
-    assert get_ag_selector(d1 - timedelta(days=2), d1) == "2016-12|2017-01"
-    assert get_ag_selector(d1 - timedelta(days=100), d1) == "2016-09|2016-10|2016-11|2016-12|2017-01"
-    assert get_ag_selector(d1 - timedelta(days=366), d1) == "2016|2017"
+    assert get_ag_selector(date1 - timedelta(days=2), date1) == "2016-12|2017-01"
+    assert get_ag_selector(date1 - timedelta(days=100), date1) == "2016-09|2016-10|2016-11|2016-12|2017-01"
+    assert get_ag_selector(date1 - timedelta(days=366), date1) == "2016|2017"
 
 def test_get_data():
     """ test query construction based on regex via ag
     """
-
-    from query import get_data, get_ag_selector
-    d = datetime(2017, 7, 15, 15, 0, 0)
+    from query import get_data
+    now = datetime(2017, 7, 15, 15, 0, 0)
 
     # 2 hosts every minute
-    assert len(get_data(d - timedelta(seconds=60), d)) == 2
-    assert len(get_data(d - timedelta(seconds=60 * 60), d)) == 60 * 2
-    assert len(get_data(d - timedelta(seconds=60 * 60 * 6), d)) == 60 * 6 * 2
+    assert len(get_data(now - timedelta(seconds=60), now)) == 2
+    assert len(get_data(now - timedelta(seconds=60 * 60), now)) == 60 * 2
+    assert len(get_data(now - timedelta(seconds=60 * 60 * 6), now)) == 60 * 6 * 2
 
 if __name__ == "__main__":
     test_get_data()
