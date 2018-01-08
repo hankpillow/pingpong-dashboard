2515dffc6031b6aa5bb4d387c35437f86e5751e6
Igor Almeida <igor.p.almeida@gmail.com>
2018-01-08 13:39:40 -0200

    clearing old code

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,11 @@ module.exports = function(env) {
 
 	env = env || process.env.NODE_ENV || 'production'
 
+	if (['production','development'].indexOf(env) === -1) {
+		console.error(`unexpected env:${env} name`)
+		process.exit(1)
+	}
+
 	let isDev = env === 'development'
 
 	let config = {
